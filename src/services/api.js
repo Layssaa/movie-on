6fd0c7bd7975b7ea72255649677c9295f0a77d9d
Layssa <layssa.lima166@gmail.com>
2021-09-29 19:18:48 -0300

    update vars

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-const PORT = 8000;
 
 const api = axios.create({
-    baseURL: `http://localhost:${PORT}`
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
