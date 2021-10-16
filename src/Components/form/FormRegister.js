69784bc16823214ef7c207f628b725012e6b7ed1
Layssa <layssa.lima166@gmail.com>
2021-10-16 17:08:00 -0300

    Update FormRegister.js

--- src/Components/form/FormRegister.js
+++ src/Components/form/FormRegister.js
@@ -19,22 +19,6 @@ export default function FormRegister(props) {
         props.history.push("/home");
     };
 
-    const SignupSchema = Yup.object().shape({
-        name: Yup.string()
-            .min(2, 'Too Short!')
-            .max(70, 'Too Long!')
-            .required('Required'),
-        email: Yup.string()
-            .email('Invalid email')
-            .required('Required'),
-        password: Yup.string()
-            .min(2, 'Too Short!')
-            .max(70, 'Too Long!')
-            .required('Required'),
-        repeatpassword: Yup.string()
-            .oneOf([Yup.ref('password'), null], 'Passwords must match'),
-    });
-
     const formik = useFormik({
         initialValues: {
             name: "",
@@ -42,7 +26,6 @@ export default function FormRegister(props) {
             password: "",
             repeatpassword: ""
         },
-        validationSchema: SignupSchema,
         onSubmit: values => {
             changePage(values);
         },
