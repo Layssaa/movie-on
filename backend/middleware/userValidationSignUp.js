9342574e1102c9d3eb74df283005b422a147e8d8
Layssa <layssa.lima166@gmail.com>
2022-07-19 00:05:05 -0300

    feat: change midd

--- backend/middleware/userValidationSignUp.js
+++ backend/middleware/userValidationSignUp.js
@@ -1,22 +1,19 @@
-const { modalUnstyledClasses } = require("@material-ui/unstyled");
-
 const userValidationSignUp = (req, res, next) => {
-    const { name, email, password, repeatpassword } = req.body;
-    try {
-        if (!name || !email || !password || !repeatpassword) {
-            throw new Error("Invalid data")
-        }
-
-        if (password !== repeatpassword) {
-            throw new Error("Different passwords")
-        }
-
-        next();
-    }
-    catch (err) {
-        res.json(err.message)
+  const { name, email, password, repeatpassword } = req.body;
+  try {
+    if (!name || !email || !password || !repeatpassword) {
+      res.send({ msg: "Invalid data", error: 400 });
     }
 
-}
+    if (password !== repeatpassword) {
+      throw Error("Different passwords");
+    }
+
+    next();
+  } catch (err) {
+    console.log("err");
+    res.send({ status: 401, error: err.message }).status(401);
+  }
+};
 
 module.exports = { userValidationSignUp };
