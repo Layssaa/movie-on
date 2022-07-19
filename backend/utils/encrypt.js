63c910c950b12bfef02bec248857d754ddb6c4c9
Layssa <layssa.lima166@gmail.com>
2022-07-19 00:06:53 -0300

    feat: up tokens

---
+++ backend/utils/encrypt.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt");
+
+async function encryptData(_token, _email) {
+  const saltRounds = 10;
+  const token = _token + _email;
+  const { hash, salt } = await madeHashPassword(token, saltRounds);
+  return { hash, salt };
+}
+
+async function madeHashPassword(_token, _saltRounds) {
+  try {
+    const salt = await bcrypt.genSalt(_saltRounds);
+    const hash = await bcrypt.hash(_token, salt);
+    return { hash, salt };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+async function authenticateUser(_token, _email, _hash) {
+  const passwordVerify = _token + _email;
+  return await bcrypt.compare(passwordVerify, _hash);
+}
+
+module.exports = { encryptData, authenticateUser };
